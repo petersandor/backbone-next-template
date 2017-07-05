84728dd83ac5fc485b1f4e2541a6abb208dc2dbf
Peter Sandor <peter@petersandor.name>
2017-07-05 12:01:09 +0200

    Experimenting with backbone decorators

--- src/views/home-page/home-page.view.ts
+++ src/views/home-page/home-page.view.ts
@@ -1,9 +1,10 @@
+import { template } from 'backbone-decorators';
+
 import { IBaseViewOptions } from '../base/base.view';
 import PageView from '../page/page.view';
 
+@template(require('./home-page.template'))
 class HomePageView extends PageView {
-
-	template: HandlebarsTemplateDelegate = require('./home-page.template');
 
 	constructor(options: IBaseViewOptions = {
 		className: 'page',
